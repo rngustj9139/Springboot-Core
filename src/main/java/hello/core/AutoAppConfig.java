18adf2b51d2b78fd3f5b6a19dc7fe32a47018c57
rngustj9139 <mnb9139@khu.ac.kr>
2022-10-12 23:59:42 +0900

    Test 에러 해결

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,15 +9,15 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
-        basePackageClasses = AutoAppConfig.class, // 해당 클레스를 포함한 패키지를 탐색한다.
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+//      basePackages = "hello.core.member", // 이 경로 아래의 파일에서만 컴포넌트 스캔을 수행한다.
+//      basePackageClasses = AutoAppConfig.class, // 해당 클레스가 컴포넌트스캔의 시작위치가 된다.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // @Configuration 어노테이션이 붙은 클래스는 빈으로 등록하지 않는다.
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() { // 수동빈이 자동빈을 오버라이딩한다.
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository") // 수동으로 등록한 빈과 자동으로 등록한 빈의 이름이 같다면
+//    MemberRepository memberRepository() { // 수동으로 등록한빈이 자동으로 등록한빈을 오버라이딩한다. (but 현재 스프링부트는 이러면 오류가 발생하게 바뀜)
+//        return new MemoryMemberRepository();
+//    }
 
 }
--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -4,7 +4,6 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
-@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
